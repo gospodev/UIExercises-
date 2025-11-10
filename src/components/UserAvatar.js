8ad85587328a22976c595f260277b58ca3700538
Informatics Admin <srv-inf-admin@unwe.bg>
2025-11-10 11:43:46 +0200

    fix(UserAvatar): import Link and use react-router links for submenu

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,20 +1,34 @@
 import React, {useState} from "react";
 import Avatar from "react-avatar";
+import { Link } from 'react-router-dom';
+import '../style/UserAvatar.css';
 
-function UserAvatar() {
+function UserAvatar( {username, AvatarMenuData} ) {
     const [avMenu, setAvMenu] = useState(false);
     const showMenu = () => setAvMenu((prev) => !prev);
 
     return (
-        <div >
+        <div className="user-avatar-wrapper">
             <Avatar
-            size="32px"
-            name="Bussiness informatics"
-            round={true}
-            onClick={showMenu}
-            textSizeRatio="1" 
+                size="32px"
+                name={username}
+                round={true}
+                onClick={showMenu}
+                textSizeRatio="1"
             />
+
+            <ul className={avMenu ? "bnav-submenu active" : "bnav-submenu"}>
+                {AvatarMenuData.map( (item, index) => (
+                    <li key={index} className="bnav-text">
+                        <Link to={item.path}>
+                        {item.icon}
+                        <div>{item.title}</div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
+
 export default UserAvatar;
